417f6711a28df76fbc009388b042bdef81b7dc9b
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-02-17 21:56:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:auth_getx/views/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterApp());
+}
+
+class FlutterApp extends StatelessWidget {
+  const FlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GetX auth',
+      theme: ThemeData(
+        primaryColor: Color(0xFF493AD5)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
